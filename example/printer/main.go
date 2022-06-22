88e264bc77d57fad355f2521d2f85e0f2c9a55e6
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-06-22 10:08:20 +0430

    feat: add example

---
+++ example/printer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/mehditeymorian/koi"
+)
+
+func main() {
+	pond := koi.NewPond()
+
+	printWorker := koi.Worker{
+		ConcurrentCount: 2,
+		QueueSize:       10,
+		Work: func(a any) any {
+			time.Sleep(1 * time.Second)
+			log.Println(a)
+
+			return nil
+		},
+	}
+
+	_ = pond.RegisterWorker("printer", printWorker)
+
+	for i := 0; i < 10; i++ {
+		_, err := pond.AddJob("printer", i)
+		if err != nil {
+			log.Printf("error while adding job: %v\n", err)
+		}
+	}
+
+	log.Println("all job added")
+
+	for {
+
+	}
+}
